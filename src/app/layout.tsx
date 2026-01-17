ed59ba5c3c55b47d742993d60b28a24d4fbc47eb
Aditya Mali <adityamali33@gmail.com>
2026-01-17 05:38:39 +0000

    the lofo is still visible of firebase in tab change it o website logo i

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,9 +19,6 @@ const ogImage = 'https://images.unsplash.com/photo-1655041448985-f6666cba2d6c?cr
 export const metadata: Metadata = {
   title: title,
   description: description,
-  icons: {
-    icon: "data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20width=%2724%27%20height=%2724%27%20viewBox=%270%200%2024%2024%27%20fill=%27none%27%20stroke=%27currentColor%27%20stroke-width=%272%27%20stroke-linecap=%27round%27%20stroke-linejoin=%27round%27%3e%3cpath%20d=%27M3%206h3%27/%3e%3cpath%20d=%27M17%206h.01%27/%3crect%20width=%2718%27%20height=%2720%27%20x=%273%27%20y=%272%27%20rx=%272%27/%3e%3ccircle%20cx=%2712%27%20cy=%2713%27%20r=%275%27/%3e%3cpath%20d=%27M12%2018a2.5%202.5%200%200%200%200-5%202.5%202.5%200%200%201%200-5%27/%3e%3c/svg%3e",
-  },
   openGraph: {
     title: title,
     description: ogDescription,
